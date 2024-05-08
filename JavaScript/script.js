9510ef2e5c75e176ef81d758863cb34ec3175b5d
ValerioL96 <v.lembo1396@gmail.com>
2024-05-08 18:09:42 +0200

    Added custom style and  underage 21% discount, seniors 42% discount

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -6,4 +6,22 @@ const userAge = prompt('Type your age');
 const costKilometers = 0.276;
 const costTicket = costKilometers * userTravKilometers;
 
-console.log(costTicket);
+
+//sconto del 21% ai minorenni
+if(userAge < 18){
+    const discount = (costTicket / 100) * 21;
+    console.log(discount);
+    document.getElementById("output").innerHTML = (discount + ' ' + '$' + '    ' + 'Underage ticket 21% discount');
+} 
+//sconto del 42% agli over 65
+else if(userAge > 65){
+    const discountTwo = (costTicket / 100) * 42;
+    console.log(discountTwo);
+    document.getElementById("output").innerHTML = (discountTwo + ' ' + '$' + '   ' + 'Seniors ticket 42% discount');
+}
+//Età compresa tra i 18 e 65 rimane invariata
+else{
+    console.log(costTicket);
+    document.getElementById("output").innerHTML = ( costTicket + ' ' + '$' + '   ' + 'Ticket');
+}
+
