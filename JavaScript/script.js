6105f4bac745ad4a9208de5414ab7486fe991435
ValerioL96 <v.lembo1396@gmail.com>
2024-05-08 17:26:07 +0200

    Added ticket cost calc

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,1 +1,9 @@
 
+const userTravKilometers = prompt('Type the km to travel');
+const userAge = prompt('Type your age');
+
+//calcolo costo biglietto
+const costKilometers = 0.276;
+const costTicket = costKilometers * userTravKilometers;
+
+console.log(costTicket);
