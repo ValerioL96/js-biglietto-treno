35f35c77a01344923a6c6639bd9e6fb85883e42c
ValerioL96 <v.lembo1396@gmail.com>
2024-05-09 12:51:09 +0200

    correction

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,27 +1,23 @@
 
-const userTravKilometers = prompt('Type the km to travel');
-const userAge = prompt('Type your age');
+const userTravKilometers = Number.parseFloat( prompt('Type the km to travel'), 10);
+const userAge = Number.parseInt( prompt('Type your age'), 10);
 
 //calcolo costo biglietto
 const costKilometers = 0.276;
-const costTicket = costKilometers * userTravKilometers;
+let discount = 0;
+
 
 
 //sconto del 21% ai minorenni
 if(userAge < 18){
-    const discount = (costTicket / 100) * 21;
-    console.log(discount);
-    document.getElementById("output").innerHTML = (discount + ' ' + '$' + '    ' + 'Underage ticket 21% discount');
+    discount = 21;
 } 
 //sconto del 42% agli over 65
 else if(userAge > 65){
-    const discountTwo = (costTicket / 100) * 42;
-    console.log(discountTwo);
-    document.getElementById("output").innerHTML = (discountTwo + ' ' + '$' + '   ' + 'Seniors ticket 42% discount');
-}
-//Età compresa tra i 18 e 65 rimane invariata
-else{
-    console.log(costTicket);
-    document.getElementById("output").innerHTML = ( costTicket + ' ' + '$' + '   ' + 'Ticket');
+    discount = 42;
 }
 
+
+let costTicket = costKilometers * userTravKilometers;
+    costTicket = costTicket - ((costTicket/100) * discount);
+    console.log(costTicket.toFixed(2), costTicket);
